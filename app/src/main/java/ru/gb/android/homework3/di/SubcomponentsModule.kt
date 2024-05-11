a4ae2eab2e48cac8ff9cfc086123be3816b37832
pkn <PKN199@yandex.ru>
2024-05-11 10:50:52 +0300

    update

--- app/src/main/java/ru/gb/android/homework3/di/SubcomponentsModule.kt
+++ app/src/main/java/ru/gb/android/homework3/di/SubcomponentsModule.kt
@@ -2,10 +2,11 @@ package ru.gb.android.homework3.di
 
 import dagger.Module
 import ru.gb.android.homework3.presentation.product.di.ProductListComponent
+import ru.gb.android.homework3.presentation.promo.di.PromoListComponent
 
 @Module(subcomponents = [
     ProductListComponent::class,
-    ProductListComponent::class,
+    PromoListComponent::class,
 
 ])
 object SubcomponentsModule
